1aa4d97fe0305e00c84c6e001b88e35517b4710c
Jasoninirio <jasonini@bu.edu>
2021-01-07 16:36:40 -0500

    Added a main page

--- drchrono_proj/drchrono_webapp/views.py
+++ drchrono_proj/drchrono_webapp/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from authlib.django.client import OAuth
+from drchrono_webapp.models import PatientInformation, DoctorInformation
 
 # USE .ENV FILE FOR API TOKENS AND KEYS
-# CLIENT_ID = 'RmxLsz3PqHVeEPBTtGw61cMVBYe8B0lLmKM7pHfV'
-# CLIENT_SECRET = '9ynHMLp9e7ZG45bkQyNbItlYLTwfyrkB2MXeaPOCMmt0W55Q73qezR7H9b2JZDrvlqFNs4FqeIE38OYnXbCmOhOc4Xn44kcpvsFcqGBsGdel9NNka6bxVs8GnGwqavu6'
-# oauth = OAuth()
-
-# oauth.register(
-#     name='DrChrono',
-#     client_id=CLIENT_ID,
-#     client_secret=CLIENT_SECRET,
-#     redirect_uri='http://127.0.0.1:8000/drchrono_webapp/home_page/callback' # replace this with postman
-#     access_token_url='https://github.com/login/oauth/access_token',
-#     authorize_url='https://github.com/login/oauth/authorize',
-#     api_base_url='https://api.github.com/',
-#     client_kwargs={'scope': 'user:email'},
-# )
 
 import datetime, pytz, requests
 
