1a982ba977712faa8c658ba45c6d07d1cebde881
Jasoninirio <jasonini@bu.edu>
2021-01-13 16:21:38 -0500

    Configuring Heroku

--- drchrono_proj/drchrono_proj/wsgi.py
+++ drchrono_proj/drchrono_proj/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drchrono_proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drchrono_proj.settings.base')
 
 application = Cling(get_wsgi_application())
