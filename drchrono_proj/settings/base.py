43f8a7520ea6dde4998c28c4e24ed34dba31d1f7
Jasoninirio <jasonini@bu.edu>
2021-01-14 17:08:27 -0500

    Updating env

--- drchrono_proj/settings/base.py
+++ drchrono_proj/settings/base.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['drdash-dev.herokuapp.com', '127.0.0.1']
-
+# ALLOWED_HOSTS = ['drdash-dev.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 # Application definition
 
