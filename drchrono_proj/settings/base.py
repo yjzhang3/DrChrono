f6169b653b37cfcd4e8fbebd30fb71e1cb3d0f15
Jasoninirio <jasonini@bu.edu>
2021-01-13 22:51:36 -0500

    Updated settings

--- drchrono_proj/settings/base.py
+++ drchrono_proj/settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'aepb5l&$l6_)_k-&drv2a_(@g6+&e37c)7i6+(y=5@v%&ryc!z'
 DEBUG = True
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['drdash-dev.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
