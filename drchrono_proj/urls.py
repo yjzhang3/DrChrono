96099da211c6bd712fb2fde84fc2c078aef26b68
Jasoninirio <jasonini@bu.edu>
2021-01-15 15:48:31 -0500

    editing urls

--- drchrono_proj/urls.py
+++ drchrono_proj/urls.py
@@ -19,7 +19,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='templates/login_page.html'), name='login')
+    path('', TemplateView.as_view(template_name='login_page.html'), name='login')
 ]
 
 # urlpatterns = [
