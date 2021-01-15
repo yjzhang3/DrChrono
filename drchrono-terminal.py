da06a73a0c56dda6a3c88ef29df4a3b3f5902683
Alperen Kitapçı <alperenmirac@gmail.com>
2021-01-16 01:10:02 +0300

    added basic surname search

--- drchrono-terminal.py
+++ drchrono-terminal.py
@@ -4,13 +4,13 @@ urllib3.disable_warnings()
 def main_app(access_token, refresh_token):
     doc_url = 'https://app.drchrono.com/api/doctors'
     json_res = make_request(doc_url, access_token)
-    doc_id = json_res['results'][1]['id']
-    doc_firstname = json_res['results'][1]['first_name']
-    doc_lastname = json_res['results'][1]['last_name']
+    doc_id = json_res['results'][0]['id']
+    doc_firstname = json_res['results'][0]['first_name']
+    doc_lastname = json_res['results'][0]['last_name']
 
     while 1:
         print('---------------\nHello, Dr. ' + doc_lastname + '!\n')
-        print('1. Print all patients (lengthy!)\n2. Print doctor information\n\n0. Exit\n\n')
+        print('1. Print all patients (lengthy!)\n2. Print doctor information\n3. Search for a patient\n\n0. Exit\n\n')
         option = input('DrDash Terminal > ')
 
         if option=='0':
@@ -19,7 +19,6 @@ def main_app(access_token, refresh_token):
             test_api_url = 'https://app.drchrono.com/api/patients'
             json_res = make_request(test_api_url, access_token)
             for i in range(len(json_res['results'])):
-                print()
                 for key, value in json_res['results'][i].items():
                     print(key, ":", value)
         elif option=='2':
@@ -29,6 +28,19 @@ def main_app(access_token, refresh_token):
                 print('\n')
                 for key, value in json_res['results'][i].items():
                     print(key, ":", value)
+        elif option=='3':
+            searchres = -1
+            searchsurname = input("Enter the surname of your patient: ")
+            test_api_url = 'https://app.drchrono.com/api/patients'
+            json_res = make_request(test_api_url, access_token)
+            for i in range(len(json_res['results'])):
+                for key, value in json_res['results'][i].items():
+                    if searchsurname==value and key=='last_name':
+                        searchres = i
+            if searchres==-1:
+                print("No results")
+            else:
+                print(json_res['results'][searchres].items())
         else:
             print('unrecognized command')
 
@@ -38,12 +50,11 @@ def make_request(url, access_token):
     api_call_headers = {'Authorization': 'Bearer ' + access_token}
     api_call_response = requests.get(url, headers=api_call_headers, verify=False)
     api_json = api_call_response.json()
-    print("Response Code", api_call_response)
     return api_json
 
-print("DrDash 1.0")
+print("DrDash 1.01")
 print("----------")
-answer = input("Are you logged in? (yes/no/debug/exit): ")
+answer = input("Are you logged in? (yes/debug/exit): ")
 
 if answer=='yes':
     access_token = input('access_token: ')
@@ -67,7 +78,7 @@ elif answer=='no':
     refresh_token = data['refresh_token']
     expires_timestamp = datetime.datetime.now(pytz.utc) + datetime.timedelta(seconds=data['expires_in'])
 elif answer=='debug':
-    main_app('deSRmIZ7Ece3ZfamL6PfOCj5CAYjI0', 't2GzQlaQpXMCwEHrfUrcOMczf6eeLk')
+    main_app('Kuy5dQbqo9jSh5SFoirdZFXBx3rpOF', 'rBer32rOY1bfrLEiCoIx0XGOVL1MIK')
 elif answer=='exit':
     print('exiting...')
 else:
