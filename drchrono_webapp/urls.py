891f6a19da63fefd968e52279a03c65b5a1ab255
Jasoninirio <jasonini@bu.edu>
2021-01-15 16:47:59 -0500

    fixed drchrono urls.py

--- drchrono_webapp/urls.py
+++ drchrono_webapp/urls.py
@@ -6,7 +6,7 @@ from drchrono_webapp.views import login_page, home_page
 
 app_name = 'drchrono'
 urlpatterns = [
-    path('login', login_page, name="login"),
-    path('home', home_page, name="home"),
+    path(r'^$', login_page, name="login"),
+    path(r'^$', home_page, name="home"),
     # path('', include('django.contrib.auth.urls')),
 ]
