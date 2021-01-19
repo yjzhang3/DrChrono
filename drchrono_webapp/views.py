823e3e5f346b73e2cc0fc93e27922b33148be649
jasoninirio <44911131+jasoninirio@users.noreply.github.com>
2021-01-19 12:42:45 -0500

    finalizing

--- drchrono_webapp/views.py
+++ drchrono_webapp/views.py
@@ -44,14 +44,11 @@ def get_response(url, access_token):
 def view_page(request):
 	if request.method == 'GET':
 		response = render(request, "login_new.html")
-		response_code = get_response(API_USER, access_token) # check if user is authorized
-		if response_code == 200 or response_code == 201 or response_code == 204:
-			request_p = request.build_absolute_uri()
-			splitted = request_p.split('code=')
-			print()
-			print("------------splitted: ", splitted)
-			print(API_DOCTOR)
-			print(API_PATIENTS)
+		request_p = request.build_absolute_uri()
+		splitted = request_p.split('code=')
+		print()
+		print("------------splitted: ", splitted)
+		if len(splitted) > 1:
 			code = splitted[1]
 			response = requests.post('https://drchrono.com/o/token/', data={
 					'code': code,
@@ -66,36 +63,36 @@ def view_page(request):
 			# Save these in your database associated with the user
 			access_token = data['access_token']
 			refresh_token = data['refresh_token']
+			response_code = get_response(API_USER, access_token) # check if user is authorized
 			print("ACCESS TOKEN: ", access_token)
 			print("REFRESH TOKEN: ", refresh_token)
 			expires_timestamp = datetime.datetime.now(pytz.utc) + datetime.timedelta(seconds=data['expires_in'])
 			print('---------CODE: ', code)
 			print('---------DATA: ', data)
 			
-			# connect api call data to data models
-			json_res = make_request(API_DOCTOR, access_token) # --!
-			print('json_res for doctor: ' ,json_res, '\n')
-			doctor, new_doc_obj = DoctorInformation.objects.update_or_create(
-				doctor_data_json=json_res
-				)
-			doctor.save()
-			print("DOCTOR INFO: ", doctor)
-			
-			json_res = make_request(API_PATIENTS, access_token) # --!
-			print('json_res for patients: ', json_res, '\n')
-			for i in range(len(json_res['results'])):
-				print('PATIENT ', i)
-				for key, value in json_res['results'][i].items():
-					print(key, ":", value)
-				patient, new_pat_obj = PatientInformation.objects.update_or_create(
-					patient_data_json=i,
-				)
-			response = render(request, "Home_jan18.html", {"user":doctor})
-			# response = render(request, "DC_Main_Page.html")
-		else:
-			response = render(request, "login_fail.html")
-	else:
-		return HttpResponseNotFound('<h1> 404: Page Not Found </h1>')
+			if response_code == 200 or response_code == 201 or response_code == 204:
+				# connect api call data to data models
+				json_res = make_request(API_DOCTOR, access_token) # --!
+				print('json_res for doctor: ' ,json_res, '\n')
+				doctor, new_doc_obj = DoctorInformation.objects.update_or_create(
+					doctor_data_json=json_res
+					)
+				doctor.save()
+				print("DOCTOR INFO: ", doctor)
+				
+				json_res = make_request(API_PATIENTS, access_token) # --!
+				print('json_res for patients: ', json_res, '\n')
+				for i in range(len(json_res['results'])):
+					print('PATIENT ', i)
+					for key, value in json_res['results'][i].items():
+						print(key, ":", value)
+					patient, new_pat_obj = PatientInformation.objects.update_or_create(
+						patient_data_json=i,
+					)
+				response = render(request, "Home_jan18.html", {"user":doctor})
+				# response = render(request, "DC_Main_Page.html")
+			else:
+				response = render(request, "login_fail.html")
 	
 	print('response: ',response)
 	return response
