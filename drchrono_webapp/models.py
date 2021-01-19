bba35566c4268512debb8f86a8b79d516546b763
jasoninirio <44911131+jasoninirio@users.noreply.github.com>
2021-01-19 13:38:23 -0500

    finalizing

--- drchrono_webapp/models.py
+++ drchrono_webapp/models.py
@@ -9,7 +9,7 @@ class DoctorInformation(models.Model):
         return (f'{self.doctor_data_json.id} {self.doctor_data_json.first_name} {self.doctor_data_json.last_name}')
 
 class PatientInformation(models.Model):
-    doctor = models.ForeignKey(DoctorInformation, on_delete=models.CASCADE, default=None)
+    doctor = models.ForeignKey(DoctorInformation, on_delete=models.CASCADE, default=JSONField(default={}))
     patient_data_json = JSONField(default={})
 
     def __str__(self):
